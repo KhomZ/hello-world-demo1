f4f4eaa5c2e5aa547438724fc2a5f5752c6d4f19
KhomZ <mgrprinc37@gmail.com>
2020-11-08 14:18:53 +0545

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Khom, how are you? This is IntelliJ IDEA");
+    }
+}
